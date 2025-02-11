f7dcd07b98bcdf9a684764d0d8f88aa0d823fb02
pranavguru <pranavguruprasad0@gmail.com>
2025-02-12 02:13:52 +0530

    restore translate multiple

--- src/control_translation/translatemultiple.py
+++ src/control_translation/translatemultiple.py
@@ -3,7 +3,9 @@ from centralized_translation import rlu, rlu_tfds, jat, torchrlds, procgen, cust
 from argparse import ArgumentParser
 import tensorflow as tf
 import gc
+import psutil
 import pickle
+import sys
 
 def build_arg_parser() -> ArgumentParser:
 
@@ -17,15 +19,43 @@ def build_arg_parser() -> ArgumentParser:
 
 def translate_shards(dataset_name, dataset_path, hf_test_data, limit_schema, output_dir):
     
-    if dataset_name=='dm_lab_rlu' or dataset_name=='dm_control_suite_rlu':
+    if dataset_name=='dm_lab_rlu_tfds' or dataset_name=='dm_control_suite_rlu_tfds':
+        
+        dirs = os.listdir(dataset_path)
+        for dir in dirs:
+            if dir!='.config':    
+                dir_path = os.path.join(dataset_path, dir)
+                #print(dir_path)
+                translated_ds = rlu_tfds(dir_path, limit_schema, output_dir, dataset_name)
+                print(f'Translated and stored file in {output_dir}')
+    
+    elif dataset_name=='dm_lab_rlu' or dataset_name=='dm_control_suite_rlu':
         
         dir_path = dataset_path
-        translated_ds = rlu_tfds(dir_path, limit_schema, output_dir, dataset_name)
-        #mod_file_path = dir_path.replace('../', '')
-        #path_to_translated = os.path.join(dataset_name+'_translated/', mod_file_path)
-        #print(path_to_translated)
-        #tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated),shard_func=custom_shard_func)
-        print(f'Translated and stored file {output_dir}')
+        # Get all files in the leaf level of dataset_path
+        all_files = []
+        for dirpath, dirnames, filenames in os.walk(dataset_path):
+            # If this is a leaf directory (no subdirectories)
+            if not dirnames:
+                # Add all non-JSON files
+                for f in filenames:
+                    if not f.endswith('.json'):
+                        all_files.append(os.path.join(dirpath, f))
+        #print(all_files)
+        for file in all_files:
+            if 'explore_object_rewards_many' in file:
+                mod_file_path = file.replace('../', '')
+                path_to_translated = os.path.join(dataset_name+'_translated/', mod_file_path)
+                #if os.path.exists(os.path.join(output_dir, path_to_translated)):
+                #    print(f'File {file} already exists in {output_dir}')
+                #    continue
+                translated_ds = rlu(file, limit_schema, output_dir, path_to_translated)
+                #tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated),shard_func=custom_shard_func)
+                print(f'Translated and stored file {file}')
+            else:
+                print(f'Skipping {file} for now')
+                continue
+        print(f'Translated and stored file in {output_dir}')
 
     elif dataset_name=='baby_ai' or dataset_name=='ale_atari' or dataset_name=='mujoco' or dataset_name=='meta_world':
 
@@ -42,12 +72,11 @@ def translate_shards(dataset_name, dataset_path, hf_test_data, limit_schema, out
         
         # Process each file
         for idx, file_path in enumerate(all_files):
-            translated_ds = jat(dataset_name,file_path, hf_test_data, limit_schema)
             mod_file_path = file_path.replace('../', '')
             path_to_translated = os.path.join(dataset_name+'_translated/', mod_file_path)
-            #print(path_to_translated)
-            tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated),shard_func=custom_shard_func)
-            print(f'Translated and stored file {file_path}')
+            jat(dataset_name,file_path, hf_test_data, limit_schema, output_dir, path_to_translated)
+
+        print(f'Translated and stored all configs of {dataset_name}')
     
     elif dataset_name=='vd4rl':
         # Get all .pt files under dataset_path
@@ -62,13 +91,19 @@ def translate_shards(dataset_name, dataset_path, hf_test_data, limit_schema, out
         
         # Process each .pt file
         for idx, file_path in enumerate(all_files):
-            translated_ds = torchrlds(file_path, dataset_name, limit_schema)
             mod_file_path = file_path.replace('../', '')
             # Remove extension if present in final component
             base, ext = os.path.splitext(mod_file_path)
             if ext:
                 mod_file_path = base
             path_to_translated = os.path.join(dataset_name+'_translated/', mod_file_path)
+
+            if os.path.exists(os.path.join(output_dir, path_to_translated)):
+                print(f'File {file_path} already exists in {output_dir}')
+                continue
+
+            translated_ds = torchrlds(file_path, dataset_name, limit_schema)
+            
             #print(path_to_translated)
             tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated), shard_func=custom_shard_func)
             print(f'Translated and stored file {file_path}')
@@ -99,61 +134,47 @@ def translate_shards(dataset_name, dataset_path, hf_test_data, limit_schema, out
     
     elif dataset_name == 'openx':
 
+        #Element spec contains the original schema of the dataset
+        pickle_path = os.path.join(output_dir, 'element_specs.pkl')
         shard_files = []
         for root, dirs, files in os.walk(dataset_path):
             shard_files = [f for f in files]
 
-            sorted_shard_files = sorted(shard_files, key=lambda x: int(x.split('_')[-1]))
+        sorted_shard_files = sorted(shard_files, key=lambda x: int(x.split('_')[-1]))
 
-            #print(sorted_shard_files)
+        #print(sorted_shard_files)
 
-            if sorted_shard_files:
-                print(f"Translating shards in {root}")
-                for idx, shard_file in enumerate(sorted_shard_files):
+        if sorted_shard_files:
+            print(f"Translating shards in {root}")
+            for idx, shard_file in enumerate(sorted_shard_files):
 
-                    shard_path = os.path.join(root, shard_file)
-                    modified_shard_path = shard_path.replace('../', '')
-                    path_to_translated = os.path.join(dataset_name+'_translated/', modified_shard_path) 
-                    #print(path_to_translated)
-
-                    if os.path.exists(os.path.join(output_dir, path_to_translated)):
-                        print(f'Translated shard {shard_path} already exists in {output_dir}')
-                        continue
-
-
-                    translated_ds = torchrlds(shard_path, dataset_name, limit_schema)
-
-                    # Extract element_spec and store in pickle
-                    # Load existing specs if file exists, otherwise create new dict
-
-                    #Element spec contains the original schema of the dataset
-                    pickle_path = os.path.join(output_dir, dataset_name+'_translated', os.path.dirname(modified_shard_path), 'element_specs.pkl')
-                    # Create directory structure if it doesn't exist
-                    os.makedirs(os.path.dirname(pickle_path), exist_ok=True)
-
-                    try:
-                        with open(pickle_path, 'rb') as f:
-                            element_specs = pickle.load(f)
-                            print(f'Element specs file found at {pickle_path}')
-                    except:
-                        element_specs = {}
-                        print(f'Element specs file not found at {pickle_path}, creating new one')
+                shard_path = os.path.join(root, shard_file)
+                translated_ds = torchrlds(shard_path, dataset_name, limit_schema)
+                
+                # Extract element_spec and store in pickle
+                # Load existing specs if file exists, otherwise create new dict
+                if os.path.exists(pickle_path):
+                    with open(pickle_path, 'rb') as f:
+                        element_specs = pickle.load(f)
+                else:
+                    element_specs = {}
+                
+                # If dataset_name not in specs, add it
+                if root.split('/')[-1] not in element_specs:
+                    element_specs[root.split('/')[-1]] = translated_ds.element_spec
                     
-                    # If dataset_name not in specs, add it
-                    if root.split('/')[-1] not in element_specs:
-                        element_specs[root.split('/')[-1]] = translated_ds.element_spec
-                        
-                        # Save updated specs
-                        with open(pickle_path, 'wb') as f:
-                            pickle.dump(element_specs, f)
-                        
-                        print(f"Element spec for {root.split('/')[-1]} added to {pickle_path}")
-                    else:
-                        print(f"Element spec for {root.split('/')[-1]} already exists in {pickle_path}")
-
-
-                    tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated), shard_func=custom_shard_func)
-                    print(f'Translated and stored file {shard_path}')
+                    # Save updated specs
+                    with open(pickle_path, 'wb') as f:
+                        pickle.dump(element_specs, f)
+                    
+                    print(f"Element spec for {root.split('/')[-1]} added to {pickle_path}")
+                else:
+                    print(f"Element spec for {root.split('/')[-1]} already exists in {pickle_path}")
+                modified_shard_path = shard_path.replace('../', '')
+                path_to_translated = os.path.join(dataset_name+'_translated/', modified_shard_path) 
+                #print(path_to_translated)
+                tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated), shard_func=custom_shard_func)
+                print(f'Translated and stored file {shard_path}')
             
             print('Translated and stored')
     
@@ -169,16 +190,18 @@ def translate_shards(dataset_name, dataset_path, hf_test_data, limit_schema, out
         if npy_files:
             print(f"Translating .npy files in {dataset_path}")
             for npy_file in npy_files:
-                translated_ds = procgen(npy_file, limit_schema)
                 # Get filename without extension for saving
                 base, ext = os.path.splitext(npy_file)
                 if ext:
                     mod_file_path = base
-                mod_file_path = mod_file_path.replace('../', '')
                 mod_file_path = os.path.join(output_dir, mod_file_path)
-
                 path_to_translated = os.path.join(dataset_name+'_translated/', mod_file_path)
-                #print(path_to_translated)
+                path_to_translated = path_to_translated.replace("../", "")
+                if os.path.exists(os.path.join(output_dir, path_to_translated)):
+                    print(f'File {npy_file} already exists in {output_dir}')
+                    continue
+                translated_ds = procgen(npy_file, limit_schema)
+                print(path_to_translated)
                 # Save translated dataset
                 tf.data.Dataset.save(translated_ds, os.path.join(output_dir, path_to_translated), shard_func=custom_shard_func)
                 print(f'Translated and stored file {npy_file}')
@@ -193,7 +216,7 @@ if __name__ == "__main__":
     
     parser = build_arg_parser()
     args = parser.parse_args()
-    translated_ds = translate_shards(args.dataset_name, args.dataset_path, args.hf_test_data, args.limit_schema, args.output_dir)
+    translate_shards(args.dataset_name, args.dataset_path, args.hf_test_data, args.limit_schema, args.output_dir)
     #Test the translated 
     '''finalds = tf.data.Dataset.load('<path to shard you want to test>')
     print(len(finalds))
