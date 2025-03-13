bae5ef4e24775afe06e7a19f5b934c6c408cd401
locke0 <yangyuewang123@gmail.com>
2025-03-12 22:46:55 -0400

    Cleans up old code, refactors profiling script, and removes redundant model loading

--- src/eval/profiling/openvla/experiments/robot/openvla_openx_eval.py
+++ src/eval/profiling/openvla/experiments/robot/openvla_openx_eval.py
@@ -24,18 +24,8 @@ import tensorflow as tf
 logger = logging.getLogger(__name__)
 # logger.setLevel(logging.DEBUG)
 
-HUBER_LOSS_SIGMA = 1.4826
 
-
-# TODO: refactor the loop 
-def get_procgen_obs(dataloader: tf.data.Dataset):
-    pass
-
-def get_openx_obs(dataloader: tf.data.Dataset):
-    pass
-
-
-def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name, use_huber_loss=False):
+def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name):
     if dataset_name in ['bigfish']:
         dataloader = get_procgen_dataloader(tfds_shards, batch_size=1)
     else:
@@ -44,17 +34,11 @@ def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name, use
     total_dataset_amse = 0.0
     action_success = []
     timestep_mse = []
-    total_huber_loss = 0.0
-
-    huber_loss_mad = 1.0
-    with open(cfg.dataset_statistics_path, "r") as f:
-        norm_stats = json.load(f)
-    huber_loss_mad = norm_stats[dataset_name]['action_mad']
-    logger.info(f"Huber loss MAD loaded: {huber_loss_mad}")
 
     obs = {}
 
     for batch in dataloader:
+        # TODO: move logic to separate procgen_eval script
         if dataset_name in ['bigfish']:
             obs_len = len(batch['observation'][0][0])
         else:
@@ -68,6 +52,7 @@ def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name, use
             mse = 0.0
 
             if dataset_name in ['bigfish']:
+                # TODO: move this into openvla_procgen_dataloader
                 # Transform Bigfish image data to correct format
                 image_data = batch['observation'][0][idx]
                 if image_data.shape == (3, 64, 64):
@@ -77,15 +62,15 @@ def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name, use
                     if obs['full_image'].dtype != np.uint8:
                         obs['full_image'] = (obs['full_image'] * 255).astype(np.uint8)
                         logger.debug("Image dtype not uint8, converted to uint8")
+
+                # Debug information
+                logger.debug(f"  Timestep {idx}:")
+                logger.debug(f"Procgen batch action: {batch['action'][0][idx]}")
+                logger.debug(f"Procgen batch reward: {batch['reward'][0][idx]}")
+                logger.debug(f"Procgen batch is_last: {batch['is_last'][0][idx]}")
+                logger.debug(f"    Image shape: {obs['full_image'].shape if obs['full_image'] is not None else 'None'}")
             else:
                 obs['full_image'] = batch['continuous_observation'][0][idx]
-
-            # Debug information
-            logger.debug(f"  Timestep {idx}:")
-            logger.debug(f"Procgen batch action: {batch['action'][0][idx]}")
-            logger.debug(f"Procgen batch reward: {batch['reward'][0][idx]}")
-            logger.debug(f"Procgen batch is_last: {batch['is_last'][0][idx]}")
-            logger.debug(f"    Image shape: {obs['full_image'].shape if obs['full_image'] is not None else 'None'}")
 
             # Check if the image is None
             if obs['full_image'] is None:
@@ -116,32 +101,29 @@ def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name, use
             mse = np.mean((np.array(standardized_predicted_action) - np.array(actual_action)) ** 2)
             timestep_mse.append(mse)
 
-            if use_huber_loss:
-                huber_loss = tf.keras.losses.Huber(
-                    delta=huber_loss_mad * HUBER_LOSS_SIGMA,
-                    reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE
-                )(standardized_predicted_action, actual_action)
-
-                total_huber_loss += huber_loss.numpy()
-
             # At the last timestep, check if the predicted action is the same as the actual action. If yes, it is considered a success
             if batch['is_last'][0][idx] == True:
-                logger.info(f"Standardized predicted action: {np.array(standardized_predicted_action)}")
-                logger.info(f"Standardized actual action: {np.array(actual_action)}")
+                logger.info(f"Episode final predicted action: {np.array(standardized_predicted_action)}")
+                logger.info(f"Episode final actual action: {np.array(actual_action)}")
                 if np.array_equal(np.array(standardized_predicted_action), np.array(actual_action)):
                     action_success.append(1)
                 else:
                     action_success.append(0)
 
 
-    action_success_rate = (sum(action_success) / len(action_success)) * 100
+    # TODO: delete after testing, should probably catch the error during production runs?
+    if len(action_success) == 0:
+        logger.warning("Action success list is empty. Defaulting to 0.0 action success rate.")
+        action_success_rate = 0.0
+    else:
+        action_success_rate = (sum(action_success) / len(action_success)) * 100
     # print(f"Action Success Rate Percentage for the dataset: {action_success_rate:.4f}")
 
     # Calculate overall average RMSE across all episodes
     total_dataset_amse = sum(timestep_mse)
     logger.info(f"\nTotal MSE across {len(timestep_mse)} timesteps: {total_dataset_amse:.4f}")
     num_timesteps = len(timestep_mse)
-    avg_dataset_amse = total_dataset_amse / num_timesteps
+    avg_dataset_amse = total_dataset_amse / num_timesteps if num_timesteps > 0 else 0.0
     
     # Calculate min-max normalized AMSE
     min_mse = min(timestep_mse)
@@ -152,9 +134,5 @@ def evaluate_openvla_model(cfg, model, processor, tfds_shards, dataset_name, use
     # print(f"Normalized Average AMSE for dataset: {normalized_amse:.4f}")
     # print(f"Timesteps for each episode: {timesteps}")
     # print(f"Total timesteps: {sum(timesteps)}")
-    
-    total_huber_loss /= HUBER_LOSS_SIGMA
-    avg_huber_loss = total_huber_loss / num_timesteps
-    logger.info(f"Total Huber Loss: {total_huber_loss:.4f}, Average Huber Loss: {avg_huber_loss:.4f}")
 
-    return action_success_rate, total_dataset_amse, avg_dataset_amse, num_timesteps, normalized_amse, total_huber_loss, avg_huber_loss
+    return action_success_rate, total_dataset_amse, avg_dataset_amse, num_timesteps, normalized_amse
